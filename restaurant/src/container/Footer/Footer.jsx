803647f0a3b249fe1c37638acfcf7f2c17418a74
unknown <jtaiwo909@gmail.com>
2023-09-18 22:21:28 +0100

    Footer Added

--- restaurant/src/container/Footer/Footer.jsx
+++ restaurant/src/container/Footer/Footer.jsx
@@ -6,8 +6,7 @@ import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div className='app__footer section__padding'>
-    <FooterOverlay />
+  <div className='app__footer app__bg section__padding'>
     <Newsletter />
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
@@ -16,10 +15,11 @@ const Footer = () => (
         <p className="p__opensans">+1 212-344-1230</p>
         <p className="p__opensans">+1 212-555-1230</p>
       </div>
+
       <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="footer-logo-image" />
+        <img src={images.footerlogo} alt="footer-logo-image" />
         <p className="p__opensans">"The best way to find yourself is to lose yourself in the service of others.”</p>
-        <img src={images.spoon} alt="spoon-image" className='spoon__img' style={{marginTop: 15}} />
+        <img src={images.spoon} alt="spoon-image" className='spoon__img' style={{marginTop: '15px'}} />
 
         <div className="app__footer-links_icons">
           <FiFacebook />
